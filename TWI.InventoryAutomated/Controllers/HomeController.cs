0b824302a9ff5291a99ecc01cd3888ac9ea3426f
Krunal Shaholia <Krunal.Shaholia@twipv.com>
2018-03-21 18:36:20 +0400

    Initial commit

---
+++ TWI.InventoryAutomated/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TWI.InventoryAutomated.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region "Global Variables"
+
+        #endregion
+
+        #region "Action Methods"
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region "Helper Function(s) "
+
+        #endregion
+
+    }
+}
---
+++ TWI.InventoryAutomated/DataAccess/CommonServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWI.InventoryAutomated.DataAccess
+{
+    public class CommonServices
+    {
+        #region  "Global Variables"
+        #endregion
+
+        #region "Constructor & Connection Function(s)"
+
+        #endregion 
+
+        #region "Helper Function(s)"
+        #endregion
+    }
+}
